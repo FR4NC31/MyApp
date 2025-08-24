dbede3de7a39a0ca2f14c8e8c279bc9d663ab214
Your Name <you@example.com>
2025-08-24 19:37:49 +0800

    Day 3: Modal, Picker, Icons, Images, Basic Animations + Mini Challenges

--- App.tsx
+++ App.tsx
@@ -1,33 +1,21 @@
-import { View, Text, Alert, TextInput, Pressable } from 'react-native'
-import React, { useState } from 'react'
+import { View, Text, Animated } from 'react-native'
+import React, { useRef, useEffect } from 'react'
 import './global.css'
 
 export default function App() {
-  const [userName, setUserName] = useState('')
-  const [password, setPassword] = useState('')
+  const fadeAnim = useRef(new Animated.Value(0)).current
 
-  const handleLogin = () => {
-    Alert.alert('Login', `Username: ${userName}\nPassword: ${password}`)
-  }
+  useEffect(() => {
+    Animated.timing(fadeAnim, {
+      toValue: 1,
+      duration: 1000,
+      useNativeDriver: true,
+    }).start()
+  }, [])
+
   return (
-    <View className='flex-1 items-center justify-center bg-gray-900'>
-      <Text className='text-2xl text-white'>Login</Text>
-      <TextInput 
-        placeholder='Username'
-        className='text-white border border-gray-700 bg-gray-800 rounded-md px-4 py-2 mt-4 w-3/4'
-        value={userName}
-        onChangeText={setUserName}
-      />
-      <TextInput 
-        placeholder='Password'
-        className='text-white border border-gray-700 bg-gray-800 rounded-md px-4 py-2 mt-4 w-3/4'
-        value={password}
-        onChangeText={setPassword}
-        secureTextEntry
-      />
-      <Pressable className='bg-blue-600 rounded-md px-4 py-2 mt-4 w-3/4 items-center' onPress={handleLogin}>
-        <Text>Login</Text>
-      </Pressable>
-    </View>
+     <Animated.View style={{ opacity: fadeAnim }} className="flex-1 justify-center items-center bg-gray-100">
+      <Text className="text-lg text-blue-500">Fade In Text!</Text>
+    </Animated.View>
   )
 }
