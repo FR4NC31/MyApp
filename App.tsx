afede80a98a8bfb8fc3321cacb4d76bee2a74b7a
Your Name <you@example.com>
2025-08-24 18:09:06 +0800

    Day 2: Layouts, ScrollView, FlatList, User Inputs + Login Form UI

--- App.tsx
+++ App.tsx
@@ -1,19 +1,32 @@
-import { View, Text, Pressable } from 'react-native'
-import React from 'react'
+import { View, Text, Alert, TextInput, Pressable } from 'react-native'
+import React, { useState } from 'react'
 import './global.css'
 
 export default function App() {
+  const [userName, setUserName] = useState('')
+  const [password, setPassword] = useState('')
 
-  const [buttonText, setButtonText] = React.useState('Press Me')
-
-  const toogleText = () => {
-    setButtonText(prev => prev === 'Press Me' ? 'Pressed!' : 'Press Me')
+  const handleLogin = () => {
+    Alert.alert('Login', `Username: ${userName}\nPassword: ${password}`)
   }
   return (
-    <View className='flex-1 items-center justify-center bg-blue-200'>
-      <Text className='text-2xl text-white mb-4'>App</Text>
-      <Pressable onPress={toogleText} className='bg-white px-4 py-2 rounded'>
-        <Text>{buttonText}</Text>
+    <View className='flex-1 items-center justify-center bg-gray-900'>
+      <Text className='text-2xl text-white'>Login</Text>
+      <TextInput 
+        placeholder='Username'
+        className='text-white border border-gray-700 bg-gray-800 rounded-md px-4 py-2 mt-4 w-3/4'
+        value={userName}
+        onChangeText={setUserName}
+      />
+      <TextInput 
+        placeholder='Password'
+        className='text-white border border-gray-700 bg-gray-800 rounded-md px-4 py-2 mt-4 w-3/4'
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+      />
+      <Pressable className='bg-blue-600 rounded-md px-4 py-2 mt-4 w-3/4 items-center' onPress={handleLogin}>
+        <Text>Login</Text>
       </Pressable>
     </View>
   )
